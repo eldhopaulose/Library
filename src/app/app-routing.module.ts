2051a4e16c18135f1b0affb0b5047e53ed3499db
Eldho Paulose <eldhopaulose0485@gmail.com>
2021-03-28 14:05:37 +0530

    book,signup,login,nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import {HomeComponent} from './home/home.component'
+import {LoginComponent} from './login/login.component'
+import {SignupComponent} from './login/signup/signup.component'
+
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home-component', component: HomeComponent },
+  { path: 'login-component', component: LoginComponent },
+  { path: 'signup-component', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './login/signup/signup.component';
+import { BookComponent } from './book/book.component';
+import { BooksComponent } from './book/books/books.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    BookComponent,
+    BooksComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
